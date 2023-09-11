a106326ebed322bc89e7903277bd2e2e99388a59
Kaviya-C <kaviyamighty04@gmail.com>
2023-09-11 19:13:33 +0530

    SQL_Basics

---
+++ Constraints_DDL.sql
@@ -0,0 +1,42 @@
+-- CONSTRAINTS
+               -- REFERS TO LIMITATIONS   or 
+               -- REFERS TO RESTRICTIONS
+               -- apply to column in a table
+
+-- constraints are very important to maintain the data integrity 
+-- among the tables
+
+--  CHECK constraint
+--  NOT NULL constraint
+--  UNIQUE constraint
+--  PRIMARY KEY  constraint combination of not null and unique
+		--  primary key only one in a table
+		--  null values not allowed
+		--  duplicates not allowed
+--  FOREIGN KEY  constriant parent child relationship 
+
+
+-- DROP 
+              -- USED TO REMOVE THE OBJECT SUCH AS VIEW ,FUNCTION,TABLE
+              -- DROP TABLE TABLE_NAME
+              -- DROP FUNCTION FUNCTION_NAEM
+              
+-- ALTER
+              -- MODIFY THE EXISTING TABLE
+              -- MODIFY THE EXISTING COLUMN NAME AND COLUMN DATA TYPE
+              -- MODIFY THE EXISTING TABLE NAME
+              -- ALTER DROP COLUMN
+              -- ALTER ADD COLUMN
+              -- ALTER TABLE TABLE_NAME RENAME TO NEW_TABLE_NAME
+              -- ALTER TABLE TABLE_NAME RENAME COLUMN COLUMN_NAME TO NEW_COLUMN_NAME
+		
+
+
+
+
+
+
+
+
+
+
---
+++ Introduction.sql
@@ -0,0 +1,47 @@
+-- SQL structured query language
+-- used to manipulate store and retrieve and many more functionalities we can do
+--  mostly used in rdbms relational database management system
+--  RDBMS is a database management system we  can store the data in table
+-- table consists of rows and columns
+-- rows is called record
+-- column is called attributes
+
+-- DDL ---> data definition language ---- CREATE ALTER DROP TRUNCATE RENAME
+
+-- DML ---> data manipulation language --- INSERT UPDATE DELETE 
+
+-- DCL ---> data control language ---- GRANT REVOKE
+
+-- DQL ----> data query language ----- SELECT
+
+-- TCL -----> transaction control language ---- COMMIT ROLLBACK SAVEPOINT 
+
+ -- most commonly used datatypes in sql
+ -- VARCHAR 
+ -- INT
+ -- BOOLEAN
+ -- CHAR
+ -- FLOAT
+ -- DATE
+ 
+ 
+ -- VARCHAR --- VARIABLES AND CHARACTERS
+		    --  allowed alphabets a-z and A-Z 
+            --  allowed numbers 0-9
+			--  combination of both alphabets and numbers
+            -- special characters 
+            --  eg: forest for13324 for$%2 
+            
+ -- INT     -- integer  values are allowed
+            -- both positive and negative values
+			-- does not store decimal values
+-- DATE --   date values in any data format 
+	    -- eg: 05-07-1985
+		-- eg: 05-july-1986
+        -- eg: 1986-07-05
+        -- eg; 05/07/1986 
+        
+-- FLOAT
+         --  holds only decimal values
+-- BOOLEAN 
+         -- stores only true or false
